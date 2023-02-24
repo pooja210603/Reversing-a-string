f2590c6320e0db5affea80c138c09109b680f0cd
Pooja <125560933+pooja210603@users.noreply.github.com>
2023-02-24 21:57:35 +0530

    Add files via upload

---
+++ EXP2_2.c
@@ -0,0 +1,22 @@
+//WAP to reverse a string given by user.
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int len;int i;
+    char str[20];
+    printf("Enter the string to be reversed \n");
+    scanf("%s",&str);
+    len=strlen(str);
+    
+    char arr[len];
+    for(i=0;i<len;i++)
+    {
+        arr[i]=str[i];
+        
+    }
+    for(i=len-1;i>=0;i--)
+    {
+        printf("%c",str[i]);
+    }
+}
